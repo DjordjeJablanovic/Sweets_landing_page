47a8fc36f45370da6a3a1e90bc889861e6544fbf
Djordje <dj.jablanovic@hotmail.com>
2022-10-28 20:58:09 +0200

    added framer motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { contactData } from '../data'
+import { fadeIn } from '../variantls';
+import {motion} from 'framer-motion'
 
 
 const Contact = () => {
@@ -8,11 +10,20 @@ const Contact = () => {
     return (
       <section className='section py-20 px-2'>
         <div className='container mx-auto'>
-          <div className='flex flex-col xl:flex-row gap-y-16'>
+          <div className='flex flex-col xl:flex-row gap-y-16 overflow-hidden'>
             {/* text */}
             
               {/* title */}
-              <h2 className='h2 max-w-[490px] text-[40px] xl:text-[85px] text-dark uppercase font-primary leading-none mb-[38px]'>{title}</h2>
+
+              <motion.div
+            variants={fadeIn('right')}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }}
+            className='flex flex-col lg:flex-row flex-1'
+          >
+                <h2 className='h2 max-w-[490px] text-[40px] xl:text-[85px] text-dark uppercase font-primary leading-none mb-[38px]'>{title}</h2>
+              </motion.div>
               {/* info items */}
               <div className='flex flex-col xl:flex-row gap-x-5 gap-y-16 xl:gap-y-0'>
                 {info.map((item, index) => {
@@ -60,28 +71,34 @@ const Contact = () => {
               </div>
            
             {/* form */}
-           
-              <form className='flex flex-col gap-y-10 w-full'>
-                <input
-                  className='border-b border-dark placeholder:text-[#555] italic tracking-[0.06em] outline-none pb-4'
-                  type='text'
-                  placeholder={form.name}
-                />
-                <input
-                  className='border-b border-dark placeholder:text-[#555] italic tracking-[0.06em] outline-none pb-4'
-                  type='text'
-                  placeholder={form.email}
-                />
-                <input
-                  className='border-b border-dark placeholder:text-[#555] italic tracking-[0.06em] outline-none pb-4 xl:mb-[30px]'
-                  type='text'
-                  placeholder={form.message}
-                />
-                <button className='self-start font-primary text-lg uppercase px-[40px] transition-all duration-300 flex items-center bg-gray-900 hover:bg-dark/90 text-white'>
-                  {form.btnText}
-                </button>
-              </form>
-            
+            <motion.div
+            variants={fadeIn('left')}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }}
+            className='flex flex-col lg:flex-row flex-1'
+          >
+                <form className='flex flex-col gap-y-10 w-full'>
+                  <input
+                    className='border-b border-dark placeholder:text-[#555] italic tracking-[0.06em] outline-none pb-4'
+                    type='text'
+                    placeholder={form.name}
+                  />
+                  <input
+                    className='border-b border-dark placeholder:text-[#555] italic tracking-[0.06em] outline-none pb-4'
+                    type='text'
+                    placeholder={form.email}
+                  />
+                  <input
+                    className='border-b border-dark placeholder:text-[#555] italic tracking-[0.06em] outline-none pb-4 xl:mb-[30px]'
+                    type='text'
+                    placeholder={form.message}
+                  />
+                  <button className='self-start font-primary text-lg uppercase px-[40px] transition-all duration-300 flex items-center bg-gray-900 hover:bg-dark/90 text-white'>
+                    {form.btnText}
+                  </button>
+                </form>
+              </motion.div>
           </div>
         </div>
       </section>
--- src/components/OurStory.jsx
+++ src/components/OurStory.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {aboutData} from '../data'
+import { fadeIn } from '../variantls';
+import {motion} from 'framer-motion'
 
 const OurStory = () => {
 
@@ -7,16 +9,29 @@ const OurStory = () => {
     return (
       <section className='lg:py-16 xl:pb-[160px] px-2'>
         <div className='container mx-auto'>
-          <div className='flex flex-col lg:flex-row gap-x-[70px] items-center'>
+          <div className='flex flex-col lg:flex-row gap-x-[70px] items-center overflow-hidden'>
             {/* numbers */}
-            
+            <motion.div
+            variants={fadeIn('right')}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }}
+            className='flex flex-col lg:flex-row flex-1'
+          >
               <div className='text-[260px] md:text-[360px] xl:text-[720px] leading-none font-tertiary lg:-tracking-[0.055em] bg-aboutus bg-no-repeat bg-right bg-clip-text text-transparent'>
                 77
               </div>
-            
+              </motion.div>
             {/* text */}
             
-              <h2 className='text-[40px] xl:text-[85px] uppercase font-primary max-w-[236px] leading-none mb-[38px]'>{title}</h2>
+            <motion.div
+            variants={fadeIn('left')}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }}
+            className='flex flex-col lg:flex-row flex-1'
+          >
+              <h2 className='text-[40px] xl:text-[85px] uppercase font-primary max-w-[236px] leading-none mb-[38px] mr-3'>{title}</h2>
               <div className='flex flex-col items-end'>
                 <div className='max-w-[530px] text-grey'>
                   <p className='mb-6'>{subtitle1}</p>
@@ -26,7 +41,7 @@ const OurStory = () => {
                   </button>
                 </div>
               </div>
-            
+              </motion.div>
           </div>
         </div>
       </section>
